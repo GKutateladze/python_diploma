26a1d8b6216853881fb4ae8042e53b96287a1b40
George <36277777+GeorgKutateladze@users.noreply.github.com>
2018-04-27 16:41:04 +0300

    Update python_diploma.py
    1. Добавлена проверка ошибок: 18 - выводит всех, кто удален или забанен; 7 - ограничение доступа; 6 - слишком много запросов.
    2.  Время: 5.20, кол-во групп 14 (не изменилось)

--- python_diploma.py
+++ python_diploma.py
@@ -4,54 +4,58 @@ import json
 
 with open('vk_token') as f:
     TOKEN = f.readline()
-
+    
 def make_request(method, **kwargs,):
     params = {
         'access_token': TOKEN,
         'v': '5.74'
     }
     params = {**params, **kwargs}
-    response = requests.get(f'https://api.vk.com/method/{method}', params)
-    return response
+    response = requests.get(f'https://api.vk.com/method/{method}', params).json()
 
-    # if response.json()['response'] == True:
-    #     return response
-    # elif response.json()['error']==True and response.json()['error']['error_code']==18:
-    #     print('User was deleted or banned')
-    #     return
-    # elif response.json()['error']==True and response.json()['error']['error_code']==10:
-    #     time.sleep(1)
-    #     return response
+    for key in response.keys():
+        if key == 'response':
+            return response
+        elif key == 'error':
+            error = response['error']
+            if error["error_code"] == 6:
+                time.sleep(2)
+                response = requests.get(f'https://api.vk.com/method/{method}', params).json()
+                return response
+            elif error["error_code"] == 7:
+                return error["error_msg"]
+            elif error["error_code"] == 18:
+                return error["error_msg"]
 
 
 def get_user(id_or_screen_name):
     user_id = make_request('users.get', user_ids=id_or_screen_name)
-    return user_id.json()['response'][0]['id']
+    return user_id['response'][0]['id']
 
 def get_friends(id_or_screen_name):
     friends = make_request('friends.get', user_id=id_or_screen_name)
-    return friends.json()
+    return friends
 
 def get_groups(id_or_screen_name):
     groups = make_request('groups.get', user_id=id_or_screen_name)
-    return groups.json()
+    return groups
 
 def get_friend_groups(user_id):
     publics = set()
     user_id = get_user(user_id)
     user_friends = get_friends(user_id)['response']['items']
     user_groups = get_groups(user_id)['response']['items']
-    time.sleep(1)
 
     for i, friend in enumerate(user_friends):
+        friend_groups = get_groups(friend)
+        
         try:
-            friend_groups = get_groups(friend)['response']['items']
+            publics.update(friend_groups['response']['items'])
+            print(i, f'vk.com/id{friend}', friend_groups)
+        except:
             publics.update(friend_groups)
             print(i, f'vk.com/id{friend}', friend_groups)
-            time.sleep(0.35)
-        except KeyError:
-            print(i, f'vk.com/id{friend} User was banned or deleted')
-
+            
     only_groups = set(user_groups) - publics
     return only_groups
 
@@ -59,22 +63,18 @@ def show_publics(user_id):
     only_groups = get_friend_groups(user_id)
     group_list = []
     for group in only_groups:
-        try:
-            only_group_json = make_request('groups.getById', group_id=group, fields='members_count').json()['response'][0]
-            time.sleep(0.35)
 
-            group_list.append({
-                'name': only_group_json['name'],
-                'gid': only_group_json['id'],
-                'members_count': only_group_json['members_count']
-            })
-            print('-*- loading -*- creating groups.json -*-')
-        except:
-            print('Oops, error occured')
+        only_group_json = make_request('groups.getById', group_id=group, fields='members_count')['response'][0]
+        group_list.append({
+            'name': only_group_json['name'],
+            'gid': only_group_json['id'],
+            'members_count': only_group_json['members_count']
+        })
+        
+        print('-*- loading -*- creating groups.json -*-')
 
     with open('groups.json', 'w', encoding='utf8') as f:
         data = json.dump(group_list, f, ensure_ascii=False, indent=2)
         return data
 
-id = 'tim_leary'
-print(show_publics(id))
+print(show_publics('tim_leary'))
